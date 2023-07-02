9deba1c8e2bdb3aad67860b91d4ee9e597ef3556
sofff24 <136091617+sofff24@users.noreply.github.com>
2023-07-02 16:37:38 +0200

    Update streamlit_app.py
    add set index to the pick the fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
  # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Choose the Fruit Name Column as the Index
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
